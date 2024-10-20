d2b577c51668597a45713141a2ba9798117238c9
Linux User <r-gith@dasiu.pl>
2024-10-20 21:16:22 +0200

    Add comments to prework.py

--- files/prework.py
+++ files/prework.py
@@ -1,3 +1,10 @@
+"""
+First (and only) pre-work solution.
+- open the given link
+- get 2 strings from the page
+- post the strings in pre-defined JSON structure to AI devs API
+The API key needs to be defined in secrets.py file.
+"""
 import requests
 import re
 import sys
@@ -6,18 +13,19 @@ import pprint
 from lib.aidevs import send_task_response
 from secrets import aidevs_api_key
 
+# Define the task parameters
 task_name = "POLIGON"
 input_link = "https://poligon.aidevs.pl/dane.txt"
 
+# Open the page and get the strings
 print (f"\nOtwieram strone: {input_link}")
-input_raw = requests.get(input_link)
-input_text = input_raw.content.decode('ascii')
-input_values = re.split("\n", input_text)
-
-answer = [i for i in input_values if i]
+input_raw = requests.get(input_link)             # The data is still in binary format
+input_text = input_raw.content.decode('ascii')   # Decode from binary to ASCII, still one string
+input_values = re.split("\n", input_text)        # Split the string by lines; last line will be empty!
+answer = [i for i in input_values if i]          # nice way of filling the array skipping the last empty line
 print ("Odnaleziono wartosci:")
 pprint.pp (answer, indent=4)
-
+# Send the strings to API (using send_task_response)
 print (f"Wysylam wartosci do API, zadanie: \"{task_name}\"")
 result = send_task_response(aidevs_api_key, task_name, answer)
 print (f"Kod odpowiedzi: {result['code']}, odpowiedz: {result['message']}")

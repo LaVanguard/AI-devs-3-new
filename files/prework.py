5bd3344cd3f4bb30ce9da5cd6a061b8bac8b305f
Linux User <r-gith@dasiu.pl>
2024-10-20 20:38:39 +0200

    Simplified prework
    - fill answer list with one-line 'for' and 'if',\n- use pprint to print the answers nicely.

--- files/prework.py
+++ files/prework.py
@@ -1,28 +1,23 @@
 import requests
 import re
 import sys
-
-from secrets import aidevs_api_key
+import pprint
 
 from lib.aidevs import send_task_response
+from secrets import aidevs_api_key
 
-input_link = "https://poligon.aidevs.pl/dane.txt"
 task_name = "POLIGON"
+input_link = "https://poligon.aidevs.pl/dane.txt"
 
-print (f"Otwieram strone: {input_link}")
+print (f"\nOtwieram strone: {input_link}")
 input_raw = requests.get(input_link)
 input_text = input_raw.content.decode('ascii')
 input_values = re.split("\n", input_text)
 
+answer = [i for i in input_values if i]
 print ("Odnaleziono wartosci:")
-answer = []
-for i in input_values:
-    if i:
-        print (f"  {i}")
-        answer.append(i)
+pprint.pp (answer, indent=4)
 
 print (f"Wysylam wartosci do API, zadanie: \"{task_name}\"")
-
 result = send_task_response(aidevs_api_key, task_name, answer)
-
 print (f"Kod odpowiedzi: {result['code']}, odpowiedz: {result['message']}")

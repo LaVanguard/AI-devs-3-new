2d19cd00171d4ebc3724c59e24f5cbdadc8e3bc7
Daniel Starnowski <r-gith@dasiu.pl>
2024-11-04 23:11:17 +0100

    Moved domain to secrets

--- files/example-secrets.py
+++ files/example-secrets.py
@@ -1,2 +1,3 @@
 aidevs_api_key = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 openai_api_key = "xxxxxxxxxxxxxxxx"
+central_domain = "domain.xyz"
--- files/task0101.py
+++ files/task0101.py
@@ -1,6 +1,6 @@
 """
 Simple solution to first task.
-- open webpage (xyz.ag3nts.org)
+- open webpage (xyz)
 - read "captcha" question
 - send request as POST
 - get the webpage
@@ -12,9 +12,9 @@ import sys
 from openai import OpenAI
 
 from lib.usedtokens import UsedTokens
-from secrets import openai_api_key
+from secrets import openai_api_key, central_domain
 
-page = "https://xyz.ag3nts.org"
+page = f"https://xyz.{central_domain}"
 username = "tester"
 password = "574e112a"
 
@@ -64,3 +64,6 @@ except Exception as error:
 result_text = result_raw.content.decode('utf8')
 print ("\nThe result page is:\n")
 print (result_text)
+flag = re.findall('{{FLG:(.*?)}}', result_text)
+if flag:
+    print (f"\n\nZnaleziono flagę: {flag[0]}")

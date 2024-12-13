724d6e1d506a06dfa272876ff23805ba26144909
Daniel Starnowski <r-gith@dasiu.pl>
2024-12-13 02:13:37 +0100

    Solution to task S04E01

---
+++ files/task0401.py
@@ -0,0 +1,138 @@
+"""
+Solution to 17th task:
+- Find 4 links to download the pictures
+- Download
+- For each of them - make a decision - DARKEN / BRIGHTEN / REPAIR if needed
+- Prepare description of Barbara based on those pictures
+"""
+
+import os
+import json
+import requests
+import base64
+from pprint import pp
+
+from lib.aidevs import send_task_response, get_response
+from sekrety import aidevs_api_key, central_domain, openai_api_key
+from lib.myai import MyAI
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+def download_file(url, file_name, download_dir='downloads'):
+    # Create download subdirectory if it doesn't exist
+    if not os.path.exists(download_dir):
+        os.makedirs(download_dir)
+    # Download the file
+    file_path = os.path.join(download_dir, file_name)
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(file_path, 'wb') as f:
+            f.write(response.content)
+        print(f"Downloaded file to {file_path}")
+    else:
+        print("Failed to download the file.")
+        return
+    return file_path
+
+print (79*"=")
+ai = MyAI(openai_api_key, True, 15)
+model = "gpt-4o-mini"
+task = "photos"
+
+# Get the links
+links_response = send_task_response(aidevs_api_key, task, "START", f"https://centrala.{central_domain}/report")
+print ("\n\nResponse with 4 links hidden:")
+pp (links_response, indent=4, width=200)
+
+# Extract the links
+system1 = """
+Your task is to write down 4 links to pictures that we want to download.
+The user's information may contain the links directly, or might contain some riddles in order to compose the links.
+Please, respond with just the 4 links, sending them in JSON list object.
+For example: ["https://link1", "https://link2", "https://link3", "https://link4"]
+"""
+messages = [
+    {"role": "system", "content": system1},
+    {"role": "user", "content": links_response['message']}
+]
+links_extracted = ai.chat_completion(messages, model, 200, 0, "Link extractor")
+print ("The links extracted:")
+links = json.loads(links_extracted)    
+pp(links, indent=4, width=200)
+print(20*"-")
+
+# Download the links
+filenames = {}
+for link in links:
+    link_split = link.split("/")
+    filenames[link_split[-1]] = link
+    download_file(link, link_split[-1])
+print ("Files downloaded!")
+
+# Process each image
+new_files = []
+system2 = """
+Your task is to make a decision whether a picture needs to be modified or repaired, or if the picture is OK.
+Please, respond with one word only, describing the next action.
+Your 4 possible options for each file are: OK, BRIGHTEN, DARKEN, REPAIR.
+If the picture has noises or glitches - respond with "REPAIR".
+If the picture is dark - respond with "BRIGHTEN".
+If the picture is boo obright - respond with "DARKEN".
+If none of above apply - please respond with "OK".
+"""
+for image in filenames:
+    image64 = encode_image(os.path.join("downloads",image))
+    messages = [
+        {"role": "system", "content": system2},
+        {"role": "user", "content": [{
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/png;base64,{image64}"
+            }
+        }]
+        }
+    ]
+    image_action = ai.chat_completion(messages, model, 50, 0, "Image repair")
+    print (f"Decision for image {image}: {image_action}")
+    if image_action != "OK":
+        links_response = send_task_response(aidevs_api_key, task, f"{image_action} {image}", f"https://centrala.{central_domain}/report")
+        print (links_response['message'])
+        system3 = f"You will receive the system message about new file to be downloaded. Please, respond only with the full URL to the file. If no domain is specified, please use the domain from the old url: {filenames[image]}"
+        messages = [
+            {"role": "system", "content": system3},
+            {"role": "user", "content": links_response['message']}
+        ]
+        new_link_response = ai.chat_completion(messages, model, 50, 0, "New link")
+        link_split = new_link_response.split("/")
+        new_files.append(link_split[-1])
+        download_file(new_link_response, link_split[-1])
+    else:
+        new_files.append(image)
+
+# Prepare for final answer
+print (40*"-")
+print ("The final list of files:")
+pp (new_files, indent=4, width=200)
+system4 = """
+Dostaniesz 4 fotografie. Proszę, znajdź wspólną postać kobiecą na tych zdjęciach.
+Następnie wypisz wszystkie cechy charakterystyczne tej kobiecej postaci, skupiając się na wszystkich szczegółach które jesteś w stanie dostrzec.
+"""
+messages = [
+    {"role": "system", "content": system4},
+    {"role": "user", "content": []}
+]
+for image in new_files:
+    image64 = encode_image(os.path.join("downloads",image))
+    messages[1]['content'].append(
+        {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{image64}"}}
+    )
+description = ai.chat_completion(messages, model, 1000, 0, "Barbara's description")
+print (40*"-")
+print ("Description of Barbara:")
+print (description)
+
+# Send final response
+final_response = send_task_response(aidevs_api_key, task, description, f"https://centrala.{central_domain}/report")
+pp (final_response, indent=4, width=200)
